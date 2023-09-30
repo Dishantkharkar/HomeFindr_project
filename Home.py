832dd269d1f4eb9b9c17cc18bcc36bc12e517d0f
Dishant Kharkar <dishantkharkar9@gmail.com>
2023-09-30 13:03:03 +0530

    Stramlit App

--- Home.py
+++ Home.py
@@ -5,6 +5,6 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Welcome to HomeFindr: Your Dream Home 👋")
 
 st.sidebar.success("Select a demo above.")
